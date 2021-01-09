47d330104eca6c0d6b2d47e221f913d3f56a218d
Vahlaz <raty.atte@gmail.com>
2021-01-09 16:21:31 +0200

    Login fail handling and listing table converted to accordion

--- frontend/src/components/ListingTable.js
+++ frontend/src/components/ListingTable.js
@@ -1,114 +1,32 @@
 import React, { useState } from 'react'
 import {
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  TableContainer,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
 } from '@material-ui/core'
-import { Box, Collapse, IconButton, Paper, Typography } from '@material-ui/core'
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
-import { makeStyles } from '@material-ui/core/styles'
 import books from '../assets/books.json'
+import { ExpandMoreOutlined } from '@material-ui/icons'
 
 const ListingTable = ({ data }) => {
-  const useRowStyles = makeStyles({
-    root: {
-      '& > *': {
-        borderBottom: 'unset',
-      },
-    },
-  })
-  const classes = useRowStyles()
-
-  const SingleRow = ({ listing }) => {
-    const [open, setOpen] = useState(false)
-    return (
-      <>
-        <TableRow key={listing.id} className={classes.root}>
-          <TableCell>
+  return (
+    <>
+      {data.map((listing) => (
+        <Accordion key={listing.id}>
+          <AccordionSummary expandIcon={<ExpandMoreOutlined />}>
             <img
-              src={books.find((a) => a.title === listing.Title).imageLink}
+              src={books.find((book) => book.title === listing.Title).imageLink}
               width='80'
               alt=''
             />
-          </TableCell>
-          <TableCell>
-            <Typography variant='subtitle1'>{listing.Title}</Typography>
-          </TableCell>
-          <TableCell>{listing.Subject}</TableCell>
-          <TableCell>{listing.Condition}</TableCell>
-          <TableCell align='right'>{listing.Price}€</TableCell>
-          <TableCell>
-            <IconButton aria-label='expand row' onClick={() => setOpen(!open)}>
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout='auto' unmountOnExit>
-              <Box margin={1}>
-                <Table size='small' aria-label='listing information'>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>{listing.Information}</TableCell>
-                      <TableCell align='right'>
-                        <h4>Myyjä:</h4>
-                      </TableCell>
-                      <TableCell align='right'>{listing.User.name}</TableCell>
-                      <TableCell align='right'>
-                        puh.{listing.User.phonenumber}
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </>
-    )
-  }
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className={'table'} aria-label='Uusimmat ilmoitukset'>
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell>
-              <Typography variant='h6'>Kirja</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant='h6'>Aine</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant='h6'>Kunto</Typography>
-            </TableCell>
-            <TableCell align='right'>
-              <Typography variant='h6'>Hinta</Typography>
-            </TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((listing) => (
-            <SingleRow listing={listing} key={listing.id} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            <Typography>{listing.Title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>ihaok</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </>
   )
 }
 export default ListingTable
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,36 +1,46 @@
 import { Button, Grid, TextField, Typography } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import { LOGIN } from './queries'
+import { Alert } from '@material-ui/lab'
+import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
   const [login, { result }] = useMutation(LOGIN)
+  const [errormessage, setErrormessage] = useState('')
 
   const handleSubmit = async (event) => {
     event.preventDefault()
     const username = event.target.username.value
     const password = event.target.password.value
-    const data = await login({
-      variables: {
-        username,
-        password,
-      },
-    })
-    if (data.error) {
-      console.log(data.error)
-    }
-
-    if (data.data.login) {
-      const { token, username, id } = data.data.login
-      console.log(username, id)
-      localStorage.setItem('KirjaKirppis-user-token', token)
+    try {
+      const data = await login({
+        variables: {
+          username,
+          password,
+        },
+      })
+      if (data.data.login) {
+        const { token, username, id } = data.data.login
+        localStorage.setItem('KirjaKirppis-user-token', token)
+        console.log(username)
+      }
+    } catch (event) {
+      console.log('kirjautuminen epäonnistui')
+      setErrormessage('Salasana tai käyttäjänimi väärin')
+      setTimeout(() => setErrormessage(''), 3000)
     }
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <Grid container justify='center' direction='column'>
-        <TextField id='username' label='Käyttäjänimi' variant='outlined' style={{ padding: 2 }}/>
+        <TextField
+          id='username'
+          label='Käyttäjänimi'
+          variant='outlined'
+          style={{ padding: 2 }}
+        />
         <TextField
           id='password'
           label='Salasana'
@@ -41,6 +51,7 @@ const LoginForm = () => {
         <Button type='submit' style={{ padding: 2 }} variant='outlined'>
           Kirjaudu sisään
         </Button>
+        {errormessage ? <Alert severity='error'>{errormessage}</Alert> : null}
       </Grid>
     </form>
   )
