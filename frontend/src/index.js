11ab1773a2aacbeef1fe9024f7cf811ab0f8f164
Atte Räty <raty.atte@gmail.com>
2025-01-22 23:27:40 +0200

    update api link

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,45 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { setContext } from 'apollo-link-context'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { setContext } from "apollo-link-context";
+import "./index.css";
 
 import {
   ApolloClient,
   ApolloProvider,
   HttpLink,
   InMemoryCache,
-} from '@apollo/client'
+} from "@apollo/client";
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('KirjaKirppis-user-token')
+  const token = localStorage.getItem("KirjaKirppis-user-token");
   return {
     headers: {
       ...headers,
       Authorization: token ? `bearer ${token}` : null,
     },
-  }
-})
+  };
+});
 const httplink = new HttpLink({
-  uri: 'https://nhc02zvzbj.execute-api.eu-central-1.amazonaws.com/dev/graphql',
-})
+  uri: "https://kirjakirppis.onrender.com",
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: authLink.concat(httplink),
-})
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <link
-      rel='stylesheet'
-      href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
     />
     <link
-      rel='stylesheet'
-      href='https://fonts.googleapis.com/icon?family=Material+Icons'
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
     />
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
